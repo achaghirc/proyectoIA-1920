b6581f91022a4eff069f3b02a866d217efe06891
Amine <aminechaghir1999@gmail.com>
2020-06-16 14:06:31 +0200

    Update archivo_a_ejeutar

--- archivo_a_ejecutar.py
+++ archivo_a_ejecutar.py
@@ -10,14 +10,20 @@ from SFS import SFS
 
 eleccion = input('¿Qué algoritmo quieres ejecutar SFS o SFFS?, introduce cuál:\n')
 archivo = input('Introduce el nombre del archivo ( Puede ser titanic.csv o BreastCancerDataset): \n')
-datos = pandas.read_csv(archivo,',')
-variables = datos.columns.tolist()
-variable_predictora = variables[len(variables)-1]
-if eleccion == 'SFFS':
-    print(SFFS.algoritmo_sffs(datos,variable_predictora))
-elif eleccion == 'SFS': 
-    D = len(variables)-2 #TODAS LAS ITERACIONES --> Excluyendo la variable resultado. 
-    print(SFS.algoritmo_sfs(datos,variable_predictora,D))
-else:
-    print('Has introducido un nombre de algoritmo incorrecto, vuelve a intentarlo las opciones son SFFS o SFS')
+
+while archivo != '' and eleccion != '': 
+    if (archivo != 'titanic.csv' or archivo != 'BreastCancerDataset'):
+        print('Has introducido un nombre de archivo incorrecto, vuelve a intentarlo con las opciones indicadas')
+        break
+    else:
+        datos = pandas.read_csv(archivo,',')
+        variables = datos.columns.tolist()
+        variable_predictora = variables[len(variables)-1]
+    if eleccion == 'SFFS':
+        print(SFFS.algoritmo_sffs(datos,variable_predictora))
+    elif eleccion == 'SFS': 
+        D = len(variables)-2 #TODAS LAS ITERACIONES --> Excluyendo la variable resultado. 
+        print(SFS.algoritmo_sfs(datos,variable_predictora,D))
+    else:
+        print('Has introducido un nombre de algoritmo incorrecto, vuelve a intentarlo las opciones son SFFS o SFS')
     
